0024c4520e5c1c6e2a55a88a3dfc226e5a82f315
blankdots <stefan@blankdots.com>
2018-01-25 15:59:01 +0200

    added plain README

---
+++ README.md
@@ -0,0 +1,10 @@
+## SHACL Validation Service
+
+The service makes use of The service makes use of https://github.com/TopQuadrant/shacl/
+
+In order to start the service
+```
+gradle run
+```
+
+Requires [RabbitMQ](https://www.rabbitmq.com).
--- src/main/java/org/uh/hulib/attx/services/validation/Main.java
+++ src/main/java/org/uh/hulib/attx/services/validation/Main.java
@@ -3,8 +3,6 @@ package org.uh.hulib.attx.services.validation;
 
 public class Main {
     public static void main(String[] args) {
-//        ValidationService webApi = new ValidationService();
-//        webApi.run();
         RPCServer app = new RPCServer();
         app.run();
     }
